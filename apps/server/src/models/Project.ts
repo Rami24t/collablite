21f3d371362c4bb69c4798dfe3f70426de5b42ad
Rami <alsaadi.rami24@gmail.com>
2026-02-03 20:58:18 +0100

    ops: create Project model (mongoose model)

---
+++ apps/server/src/models/Project.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IProject extends Document {
+  name: string;
+  description?: string;
+  owner: Types.ObjectId;
+  members: Types.ObjectId[];
+}
+
+const ProjectSchema = new Schema<IProject>({
+  name: { type: String, required: true },
+  description: String,
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+export const Project = mongoose.model<IProject>("Project", ProjectSchema);
